d52cbee6cbfafc1ff9bfb9738ead7eefe3c036b0
LucasNZ <trabalhovba@gmail.com>
2025-06-03 13:10:28 -0300

    Feat: 100%

--- views/listUsers.php
+++ views/listUsers.php
@@ -38,11 +38,11 @@
                             <div class="text-sm text-gray-900"><?= (new Unidade())->idUnidade($usuario->id_unidade)->unidade; ?></div>
                         </td>
                         <td data-label="Tipo de Acesso" class="px-6 py-4 whitespace-nowrap">
-                            <div class="text-sm text-gray-900"><?= $usuario->tipo_acesso; ?></div>
+                            <div class="color-user text-sm text-blue-800 bg-blue-200 rounded-full py-0.5"><?= $usuario->tipo_acesso; ?></div>
                         </td>
                         <td data-label="Status" class="px-6 py-4 whitespace-nowrap">
                             <span class="status-badge px-2.5 py-0.5 rounded-full text-xs font-medium bg-green-100 text-green-800">
-                                <?= $usuario->ativo ? "ativo" : "cancelado" ; ?>
+                                <?= $usuario->ativo ? "ativo" : "inativo" ; ?>
                             </span>
                         </td>
                         <td data-label="Ação" class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
